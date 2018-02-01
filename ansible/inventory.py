bf2dd9407582bd4a63d7e3e8bee747b72b750802
Timur Lapshin <t.lapshin@gmail.com>
2018-02-01 15:41:08 +0000

    Homework 10

---
+++ ansible/inventory.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import sys
+import json
+
+if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
+    inventory=open('inventory.json','r')
+    print json.dumps(json.load(inventory), indent=4)
+elif len(sys.argv) == 3 and (sys.argv[1] == '--host'):
+    print '{}'
+else:
+    print "Usage: %s --list or --host <hostname>" % sys.argv[0]
+    sys.exit(1)
